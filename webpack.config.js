2c1cd91228d832428e8ef5109ed0cc689ca80036
Daniel Bevziuc <danielbevziuc@Daniels-MacBook-Pro.local>
2020-05-06 15:41:18 +0100

    add Eslint

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,23 @@ const isDev =!isProd
 
 const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
 
+const jsLoader = () => {
+    const loaders = [
+        {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env']
+            }
+        }
+    ]
+
+    if (isDev) {
+        loaders.push('eslint-loader')
+    }
+
+    return loaders
+}
+
 module.exports = {
     context : path.resolve(__dirname, 'src'),
     mode: 'development',
@@ -54,7 +71,13 @@ module.exports = {
             {
                 test: /\.s[ac]ss$/i,
                 use: [
-                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: isDev,
+                            reloadAll: true
+                        }
+                    },
                     'css-loader',
                     'sass-loader',
                 ],
@@ -62,12 +85,7 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
+                use: jsLoader(),
             },
         ],
     },
